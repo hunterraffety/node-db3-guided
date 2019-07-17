74cdc2b3ee1b00a76a91bc061a9e50be79e164ec
hsrsbd <hsraffety@gmail.com>
2019-07-17 11:25:38 -0700

    final commit.

--- users/user-router.js
+++ users/user-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const db = require('../data/db-config.js');
+const db = require('../data/db-config');
+const Users = require('./user-model');
 
 const router = express.Router();
 
@@ -11,8 +12,16 @@ const router = express.Router();
 //   res.status(500).json({ message: 'Failed to get users' });
 // }
 
+// db('users')
+//   .then(users => {
+//     res.status(200).json(users);
+//   })
+//   .catch(error => {
+//     res.status(500).json(error);
+//   });
+
 router.get('/', async (req, res) => {
-  db('users')
+  Users.find()
     .then(users => {
       res.status(200).json(users);
     })
@@ -21,20 +30,31 @@ router.get('/', async (req, res) => {
     });
 });
 
+// try {
+//   const [user] = await db('users').where({ id });
+
+//   if (user) {
+//     res.json(user);
+//   } else {
+//     res.status(404).json({ message: 'Could not find user with given id.' });
+//   }
+// } catch (err) {
+//   res.status(500).json({ message: 'Failed to get user' });
+// }
+
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-
-  try {
-    const [user] = await db('users').where({ id });
-
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'Could not find user with given id.' });
-    }
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to get user' });
-  }
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/', async (req, res) => {
@@ -83,6 +103,16 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: 'Failed to delete user' });
   }
+});
+
+router.get('/:id/posts', (req, res) => {
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
